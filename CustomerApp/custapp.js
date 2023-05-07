eae3092c3c102443a92446ecb77dd693d288a697
Aarushi Sinha <84239681+techna08@users.noreply.github.com>
2023-05-06 23:41:04 -0400

    Update custapp.js

--- CustomerApp/custapp.js
+++ CustomerApp/custapp.js
@@ -6,11 +6,7 @@ const mysql = require('mysql');
 const custController = require('./controllers/custController');
 
 const connection = mysql.createConnection({
-  // host: "localhost",
-  // port: "3306",
-  // user: "root",
-  // password: "Ipad@1212",
-  // database: "BookStore",
+
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
